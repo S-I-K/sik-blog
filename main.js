5eec935c4b49f80e14424f8d02c9722be9e404f7
SIK <daesik99@naver.com>
2022-11-30 00:43:00 +0900

    update/ box-shadow, works section

---
+++ main.js
@@ -0,0 +1,10 @@
+// let a = document.querySelectorAll('a');
+// console.log(a);
+
+// function LinkEventRemove(e){
+//     e.preventDefault();
+// }
+
+// for(let i=0; i<a.length; i++){
+//     a[i].addEventListener('click', LinkEventRemove);
+// };
