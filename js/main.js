3d4c4c43ee95d124eb3937f67ff75eaa4eb0d5cf
SIK <daesik99@naver.com>
2023-01-09 00:48:38 +0900

    toggle switch code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+/* a 태그 기본옵션 제거 */
 let a = document.querySelectorAll('a');
 for(let i=0; i<a.length; i++) {
     a[i].addEventListener("click", (e)=>{
         e.preventDefault();
     });
 }
+
+/* 코드 이게 최선인가 리펙토링 필요해보임 */
+$(()=>{
+    let toggleWrap = $('.toggle-wrap');
+    toggleWrap.click(function(){
+        $(this).toggleClass('active');
+        if($(this).hasClass('active')){
+            $('.license').show();
+            $('.school').hide();
+        }else{
+            $('.school').show();
+            $('.license').hide();
+        }
+    });
+});
