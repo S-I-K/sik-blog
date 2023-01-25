d07cadcdc06b5684824db8f92f3726879b769a1d
SIK <daesik99@naver.com>
2023-01-26 01:40:36 +0900

    once implement scroll progress number animate

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ for (let i = 0; i < a.length; i++) {
     e.preventDefault();
   });
 }
+
 /* jquery */
 $(() => {
-  /* 리펙토링 필요해보임 */
+  /* toggle switch: 리펙토링 필요해보임 */
   let toggleWrap = $(".toggle-wrap");
   toggleWrap.click(function () {
     $(this).toggleClass("active");
@@ -19,6 +20,63 @@ $(() => {
       $(".license").hide();
     }
   });
+
+  /* how to use animate();
+    $('.box').animate({속성:값, 속성:값}, duration, easing, func(){});
+
+    $('.box').animate({속성: 값, ...}, {
+      duration: ,
+      easing: ,
+      complate: function(){...} //끝나고 할 일
+      or
+      prgoress: function(){...} //진행중에 할 일
+    });
+  */
+  
+  let executed = false; //false 일때만 scroll event 실행
+  $(window).scroll(function(){
+    let threshold = $('.skill').offset().top - 300 ;
+    console.log("현재 skill section의 위치값: "+threshold);
+    console.log("현재 scroll: "+$(window).scrollTop());
+    if(!executed){
+      if($(window).scrollTop() >= threshold){
+        $(".progress").each(function () {
+          /* 각각의 data-rate 옵션을 변수에 저장 */
+          let $this = $(this),
+              progressRate = $this.attr("data-rate");
+          /* animate 함수를 이용해서 숫자가 차례차례 올라가게 함 */
+          /* 임의의 함수 rate의 초기값을 정해주고 rate가 목표치까지 증가 */
+          $({ rate: $this.text() }).animate({rate: progressRate},{
+              duration: 2000,
+              easing: "linear",
+              /* step이라는 기명함수로 각각의 숫자($this)에 목표치 숫자를 대입하여 UI에 표현 */
+              step: function () {
+                $this.text(Math.ceil(this.rate) + "%");
+              },
+            }
+          );
+        });
+        executed = true;
+      }
+    } //end: executed if
+  });
+
+
+
+
+  /* header global navigation bar function */
+  $(".gnb > li > a").click(function () {
+    $("html,body")
+      .stop()
+      .animate(
+        { scrollTop: $(this.hash).offset().top },
+        {
+          duration: 800,
+          easing: "easeInOutQuad",
+        }
+      );
+  });
+
   /* swiper.js: slide */
   var work = new Swiper("#work", {
     slidesPerView: "auto",
@@ -31,40 +89,6 @@ $(() => {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
-
-  /* 숫자 증가 애니메이션 */
-  $(".progress").each(function () {
-    /* 각각의 data-rate 옵션을 변수에 저장 */
-    let $this = $(this),
-      dataRate = $this.attr("data-rate");
-
-    /* animate 함수를 이용해서 숫자가 차례차례 올라가게 함 */
-    /* 임의의 함수 rate의 초기값을 정해주고 rate가 목표치까지 증가 */
-    $({ rate: $this.text() }).animate(
-      { rate: dataRate },
-      {
-        duration: 1000,
-        easing: "linear",
-        /* step이라는 기명함수로 각각의 숫자($this)에 목표치 숫자를 대입하여 UI에 표현 */
-        step: function () {
-          $this.text(Math.ceil(this.rate) + "%");
-        },
-      }
-    );
-  });
-
-  /* gnb */
-  $(".gnb > li > a").click(function () {
-    $("html,body")
-      .stop()
-      .animate(
-        { scrollTop: $(this.hash).offset().top },
-        {
-          duration: 800,
-          easing: "easeInOutQuad",
-        }
-      );
   });
 
   /* scrollToGiveClass */
