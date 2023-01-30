d1486fd303f48237e1c858c3a5657895016aedb1
sik <daesik99@naver.com>
2023-01-30 16:17:47 +0900

    modified/ intro: comment typing animation, html: scrollbar, footer: background color

--- js/main.js
+++ js/main.js
@@ -5,6 +5,48 @@ for (let i = 0; i < a.length; i++) {
     e.preventDefault();
   });
 } */
+/* typing animation */
+const $comment = document.querySelector('.comment');
+// 글자 모음
+const letters = [
+  "상상한 것을 그래픽으로 표현하는 디자이너",
+  "화면을 스케치북처럼 다룰 줄 아는 디자이너",
+  "프론트엔드를 꿈꾸는 디자이너"
+];
+// 글자 입력 속도
+const speed = 100;
+let i = 0;
+// 타이핑 효과
+const typing = async () => {  
+  const letter = letters[i].split("");
+  while (letter.length) {
+    await wait(speed);
+    $comment.innerHTML += letter.shift(); 
+  }
+  // 잠시 대기
+  await wait(800);
+  // 지우는 효과
+  remove();
+}
+// 글자 지우는 효과
+const remove = async () => {
+  const letter = letters[i].split("");
+  while (letter.length) {
+    await wait(speed);
+    
+    letter.pop();
+    $comment.innerHTML = letter.join(""); 
+  }
+  // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
+  i = !letters[i+1] ? 0 : i + 1;
+  typing();
+}
+// 딜레이 기능 ( 마이크로초 )
+function wait(ms) {
+  return new Promise(res => setTimeout(res, ms))
+}
+// 초기 실행
+setTimeout(typing, 1500);
 
 /* jquery */
 $(() => {
